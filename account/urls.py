12f4185880537dd317c753dfffdabbf614b7e890
Syl9w <ahmetkalievsultan@gmail.com>
2021-08-04 12:50:55 +0600

    first commit

---
+++ account/urls.py
@@ -0,0 +1,41 @@
+from django import contrib
+from django.contrib import auth
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+urlpatterns = [
+    #path('login/', user_login, name='login' ),
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
+"""
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    #password change
+    path('password_change/', 
+            auth_views.PasswordChangeView.as_view(), 
+            name='password_change'),
+    path('password_change/done/', 
+            auth_views.PasswordChangeDoneView.as_view(), 
+            name='password_change_done'),
+    #password reset
+    
+
+    path('reset/', 
+            auth_views.PasswordResetView.as_view(), 
+            name="password_reset"),
+    path('reset/done/', 
+            auth_views.PasswordResetDoneView.as_view(), 
+            name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', 
+            auth_views.PasswordResetConfirmView.as_view(),
+            name='password_reset_confirm'),
+    path('reset/complete/',
+            auth_views.PasswordResetCompleteView.as_view(),
+            name='password_reset_complete'),
+]
+"""
+
